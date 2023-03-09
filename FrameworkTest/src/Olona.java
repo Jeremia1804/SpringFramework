b585f40c0ee4740b98b2e861e8e854163d5d28b4
Jeremia1804 <hasina20.jeremia@gmail.com>
2023-03-09 11:50:06 +0300

    Sprint3

---
+++ FrameworkTest/src/Olona.java
@@ -0,0 +1,19 @@
+package pers;
+import annote.Url;
+public class Olona {
+
+    @Url("ecouteur")
+    public void findById(){
+
+    }
+
+    @Url("ecouteur1")
+    public void findAll(){
+
+    }
+
+    @Url("ecouteur2")
+    public void save(){
+ 
+    }
+}
